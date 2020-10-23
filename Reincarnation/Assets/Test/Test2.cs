6d45c4961a0684d9f3e5e822b592e11fc629c06c
pm83126492 <pm83126492@gmail.com>
2020-10-24 00:33:13 +0800

    Add SwingScript_01
    SwingScript_01

---
+++ Reincarnation/Assets/Test/Test2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        gameObject.GetComponent<Rigidbody2D>().AddForce(transform.forward * y);
+        gameObject.GetComponent<Rigidbody2D>().AddForce(transform.right * x);
+    }
+}
